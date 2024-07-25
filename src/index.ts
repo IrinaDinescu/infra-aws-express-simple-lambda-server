15574001e3faeb6b9e35f68188ebf760bc97f6a8
Irina Dinescu <irina.dinescu27@gmail.com>
2024-07-25 19:57:14 +0300

    added cloud watch logs

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello!');
 });
 
+app.get('/hello', (req: Request, res: Response) => {
+  console.log('/hello route was hit!');
+  res.send('Hello from hello!');
+});
+
 // Start the server if running locally
 if (process.env.NODE_ENV !== 'lambda') {
   app.listen(PORT, () => {
